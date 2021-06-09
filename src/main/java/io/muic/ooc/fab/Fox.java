e4fbb1c817cedc88a089b306e295afba2a85ca93
Rathanon <rathanon.sin@student.mahidol.edu>
2021-06-09 14:58:02 +0700

    removed variables (max age, breeding etc)

--- src/main/java/io/muic/ooc/fab/Fox.java
+++ src/main/java/io/muic/ooc/fab/Fox.java
@@ -6,15 +6,6 @@ import java.util.Random;
 
 public class Fox extends Animal {
     // Characteristics shared by all foxes (class variables).
-
-    // The age at which a fox can start to breed.
-    private static final int BREEDING_AGE = 15;
-    // The age to which a fox can live.
-    private static final int MAX_AGE = 150;
-    // The likelihood of a fox breeding.
-    private static final double BREEDING_PROBABILITY = 0.08;
-    // The maximum number of births.
-    private static final int MAX_LITTER_SIZE = 2;
     // The food value of a single rabbit. In effect, this is the
     // number of steps a fox can go before it has to eat again.
     private static final int RABBIT_FOOD_VALUE = 9;
@@ -111,12 +102,12 @@ public class Fox extends Animal {
 
     @Override
     protected int getMaxLitterSize() {
-        return MAX_LITTER_SIZE;
+        return 2;
     }
 
     @Override
     protected int getBreedingAge() {
-        return BREEDING_AGE;
+        return 15;
     }
 
     @Override
