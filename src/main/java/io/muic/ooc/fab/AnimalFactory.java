6ea36068a762e608fd251b9d6f14c877db7078c9
Rathanon <rathanon.sin@student.mahidol.edu>
2021-06-09 20:35:14 +0700

    add animal class

--- src/main/java/io/muic/ooc/fab/AnimalFactory.java
+++ src/main/java/io/muic/ooc/fab/AnimalFactory.java
@@ -1,6 +1,19 @@
 package io.muic.ooc.fab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AnimalFactory {
+
+    private static Map<AnimalType, Double> probabilityMap = new HashMap<AnimalType, Double>(){{
+        AnimalType[] animalTypes = AnimalType.values();
+        for (int i = 0; i < animalTypes.length; i++) {
+            put(animalTypes[i], animalTypes[i].getBreedingProbability());
+
+        }
+    }};
+
+
     public static Animal createAnimal(AnimalType animalType, Field field, Location location) {
         switch (animalType) {
             case RABBIT:
--- src/main/java/io/muic/ooc/fab/AnimalType.java
+++ src/main/java/io/muic/ooc/fab/AnimalType.java
@@ -1,16 +1,24 @@
 package io.muic.ooc.fab;
 
 public enum AnimalType {
-    RABBIT(0.08),
-    FOX(0.02);
+    RABBIT(0.08, Rabbit.class),
+    FOX(0.02, Fox.class);
 
     private double breedingProbability;
 
-    AnimalType(double breedingProbability) {
+    private Class animalClass;
+
+    AnimalType(double breedingProbability, Class animalClass) {
         this.breedingProbability = breedingProbability;
+        this.animalClass = animalClass;
     }
 
     public double getBreedingProbability() {
         return breedingProbability;
     }
+
+    public Class getAnimalClass() {
+        return animalClass;
+    }
+
 }
