ac3b7c1222ad6bd0b8c325726ac806347466acf3
Rathanon <rathanon.sin@student.mahidol.edu>
2021-06-12 22:08:34 +0700

    Working Hunter (Killing everyone)

--- src/main/java/io/muic/ooc/fab/AnimalType.java
+++ src/main/java/io/muic/ooc/fab/AnimalType.java
@@ -5,7 +5,8 @@ import java.awt.*;
 public enum AnimalType {
     RABBIT(0.08, Rabbit.class, Color.ORANGE),
     FOX(0.04, Fox.class, Color.BLUE),
-    TIGER(0.02, Tiger.class, Color.RED);
+    TIGER(0.02, Tiger.class, Color.RED),
+    HUNTER(0.01, Hunter.class, Color.GREEN);
     private double breedingProbability;
 
     private Class animalClass;
